763f43a5bd6a968382bf6d90c2783986924d75d7
Shashwat Singh <shashwat123student@gmail.com>
2023-10-29 13:00:23 +0530

    Finale

--- LandingPage/src/Components/Snake.jsx
+++ LandingPage/src/Components/Snake.jsx
@@ -1,15 +1,28 @@
 import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import {StaticNavBar2} from "./Navbar2.jsx";
 
-// const ogValue = `
-// - uses: Platane/snk@v3
-//   with:
-//     github_user_name: ${{ github.repository_owner }}
-//     outputs: |
-//       dist/github-snake.svg
-//       dist/github-snake-dark.svg?palette=github-dark
-//       dist/ocean.gif?color_snake=orange&color_dots=#bfd6f6,#8dbdff,#64a1f4,#4b91f1,#3c7dd9;
-// `;
+const ogValue = `
+- uses: Platane/snk@v3
+  with:
+    # github user name to read the contribution graph from (**required**)
+    # using action context var \`github.repository_owner\` or specified user
+    github_user_name: \${{ github.repository_owner }}
+
+    # list of files to generate.
+    # one file per line. Each output can be customized with options as query string.
+    #
+    #  supported options:
+    #  - palette:     A preset of color, one of [github, github-dark, github-light]
+    #  - color_snake: Color of the snake
+    #  - color_dots:  Coma separated list of dots color.
+    #                 The first one is 0 contribution, then it goes from the low contribution to the highest.
+    #                 Exactly 5 colors are expected.
+    outputs: |
+      dist/github-snake.svg
+      dist/github-snake-dark.svg?palette=github-dark
+      dist/ocean.gif?color_snake=orange&color_dots=#bfd6f6,#8dbdff,#64a1f4,#4b91f1,#3c7dd9
+`;
 
 function Snake() {
     const editorRef = useRef();
@@ -21,6 +34,8 @@ function Snake() {
     };
 
     return (
+        <div>
+            <StaticNavBar2></StaticNavBar2>
         <div style={{ padding: "30px" }}>
             <Editor
                 onInit={(evt, editor) => {
@@ -40,6 +55,7 @@ function Snake() {
                 </ol>
             </div>
             <button onClick={updateEditorContent}>Load Default Content</button>
+        </div>
         </div>
     );
 }
