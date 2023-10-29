1c7cdf3e41edb5a4b9c4c4186d2772e41c3fd9d2
Shashwat Singh <shashwat123student@gmail.com>
2023-10-29 08:39:24 +0530

    Merged

--- LandingPage/src/Components/Dashboard.jsx
+++ LandingPage/src/Components/Dashboard.jsx
@@ -86,105 +86,130 @@ const Dashboard = () => {
                     <Avatar alt="Remy Sharp" src="src\assets\avatar.jpg" style={{ marginRight: 2, marginTop: 1.5, borderRadius: "30px", border: "2px solid grey" }} />
                 </Toolbar>
             </AppBar>
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Box m="20px">
-                    <Box display="flex" justifyContent="space-between" alignItems="center">
-                        <Box>
-                            <Button
-                                sx={{
-                                    backgroundColor: colors.blueAccent[700],
-                                    color: colors.grey[100],
-                                    fontSize: "14px",
-                                    fontWeight: "bold",
-                                    padding: "10px 20px",
-                                }}
-                                onClick={()=>navigate("/tools")}>
-                                View Tools
-                            </Button>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box m="20px">
+                        <Box display="flex" justifyContent="space-between" alignItems="center">
+                            <Box>
+                                <Button
+                                    sx={{
+                                        backgroundColor: colors.blueAccent[700],
+                                        color: colors.grey[100],
+                                        fontSize: "14px",
+                                        fontWeight: "bold",
+                                        padding: "10px 20px",
+                                    }}
+                                    onClick={()=>navigate("/tools")}>
+                                    View Tools
+                                </Button>
+                            </Box>
                         </Box>
-                    </Box>
 
-                    <Box
-                        display="grid"
-                        gridTemplateColumns="repeat(12, 1fr)"
-                        gridAutoRows="140px"
-                        gap="20px"
-                    >
-                        {fork}
                         <Box
-                            gridColumn="span 3"
-                            backgroundColor={colors.primary[400]}
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
+                            display="grid"
+                            gridTemplateColumns="repeat(12, 1fr)"
+                            gridAutoRows="140px"
+                            gap="20px"
                         >
-                            <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}>Total Forks</Typography>
-                            <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{fork}</h1>
-                            <div style={{}}>
-                                <h1><ForkLeftIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></ForkLeftIcon></h1>
-                            </div>
-                        </Box>
-                        <Box
-                            gridColumn="span 3"
-                            backgroundColor={colors.primary[400]}
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                        >
-                            <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}> Total Stars</Typography>
-                            <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{stars}</h1>
-                            <div style={{}}>
-                                <h1><StarIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></StarIcon></h1>
-                            </div>
-                        </Box>
-                        <Box
-                            gridColumn="span 3"
-                            backgroundColor={colors.primary[400]}
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                        >
-                            <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}> Total Followers</Typography>
-                            <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{followers}</h1>
-                            <div style={{}}>
-                                <h1><PeopleIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></PeopleIcon></h1>
-                            </div>
-                        </Box>
-                        <Box
-                            gridColumn="span 3"
-                            backgroundColor={colors.primary[400]}
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                        >
-                            <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}>Following</Typography>
-                            <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{following}</h1>
-                            <div style={{}}>
-                                <h1><PeopleIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></PeopleIcon></h1>
-                            </div>
-
-                        </Box>
-                        <Box
-                            gridColumn="span 8"
-                            gridRow="span 2"
-                            backgroundColor={colors.primary[400]}
-                        >
+                            {fork}
                             <Box
-                                mt="25px"
-                                p="0 30px"
-                                display="flex "
-                                justifyContent="space-between"
+                                gridColumn="span 3"
+                                backgroundColor={colors.primary[400]}
+                                display="flex"
                                 alignItems="center"
+                                justifyContent="center"
                             >
-                                <div style={{marginTop : "-6.5vh"}}>
-                                    <h1 style={{paddingBottom : "0.5vh"}}>Top 15 repos</h1>
+                                <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}>Total Forks</Typography>
+                                <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{fork}</h1>
+                                <div style={{}}>
+                                    <h1><ForkLeftIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></ForkLeftIcon></h1>
+                                </div>
+                            </Box>
+                            <Box
+                                gridColumn="span 3"
+                                backgroundColor={colors.primary[400]}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}> Total Stars</Typography>
+                                <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{stars}</h1>
+                                <div style={{}}>
+                                    <h1><StarIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></StarIcon></h1>
+                                </div>
+                            </Box>
+                            <Box
+                                gridColumn="span 3"
+                                backgroundColor={colors.primary[400]}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}> Total Followers</Typography>
+                                <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{followers}</h1>
+                                <div style={{}}>
+                                    <h1><PeopleIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></PeopleIcon></h1>
+                                </div>
+                            </Box>
+                            <Box
+                                gridColumn="span 3"
+                                backgroundColor={colors.primary[400]}
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Typography style={{marginLeft : "-10vh" , marginTop : "-13vh"}}>Following</Typography>
+                                <h1 style={{marginLeft : "5vh" , color : "Black" , fontSize : "7vh"}}>{following}</h1>
+                                <div style={{}}>
+                                    <h1><PeopleIcon style={{fontSize : "6vh"  , position : "relative" , left : "10vh" , top : "-3vh"}}></PeopleIcon></h1>
+                                </div>
+
+                            </Box>
+                            <Box
+                                gridColumn="span 8"
+                                gridRow="span 2"
+                                backgroundColor={colors.primary[400]}
+                            >
+                                <Box
+                                    mt="25px"
+                                    p="0 30px"
+                                    display="flex "
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                >
+                                    <div style={{marginTop : "-6.5vh"}}>
+                                        <h1 style={{paddingBottom : "0.5vh"}}>Top 15 repos</h1>
+                                        <Box
+                                            sx={{ marginTop : "-3.5vh" ,  width: '100%', height: 300, maxWidth: 1100 }}>
+                                            <FixedSizeList
+                                                height={250}
+                                                width={930}
+                                                itemSize={46}
+                                                itemCount={200}
+                                                overscanCount={5}
+                                            >
+                                                {renderRow}
+                                            </FixedSizeList>
+                                        </Box>
+                                    </div>
+                                </Box>
+                                <Box height="250px" m="-20px 0 0 0">
+                                </Box>
+                            </Box>
+                            <Box
+                                gridColumn="span 4"
+                                gridRow="span 2"
+                                backgroundColor={colors.primary[400]}
+                                overflow="auto"
+                            >
+                                <div style={{marginTop : "-1.5vh"}}>
+                                    <h1 style={{paddingBottom : "0.5vh"}}>Recent Forks</h1>
                                     <Box
-                                        sx={{ marginTop : "-3.5vh" ,  width: '100%', height: 300, maxWidth: 1100 }}>
+                                        sx={{ marginTop : "-1.5vh" ,  width: '100%', height: 240, maxWidth: 480 }}>
                                         <FixedSizeList
-                                            height={250}
-                                            width={930}
+                                            height={240}
+                                            width={480}
                                             itemSize={46}
                                             itemCount={200}
                                             overscanCount={5}
@@ -194,91 +219,66 @@ const Dashboard = () => {
                                     </Box>
                                 </div>
                             </Box>
-                            <Box height="250px" m="-20px 0 0 0">
-                            </Box>
-                        </Box>
-                        <Box
-                            gridColumn="span 4"
-                            gridRow="span 2"
-                            backgroundColor={colors.primary[400]}
-                            overflow="auto"
-                        >
-                            <div style={{marginTop : "-1.5vh"}}>
-                                <h1 style={{paddingBottom : "0.5vh"}}>Recent Forks</h1>
-                                <Box
-                                    sx={{ marginTop : "-1.5vh" ,  width: '100%', height: 240, maxWidth: 480 }}>
-                                    <FixedSizeList
-                                        height={240}
-                                        width={480}
-                                        itemSize={46}
-                                        itemCount={200}
-                                        overscanCount={5}
-                                    >
-                                        {renderRow}
-                                    </FixedSizeList>
-                                </Box>
-                            </div>
-                        </Box>
 
-                        {followers}
-                        <Box
-                            gridColumn="span 4"
-                            gridRow="span 2"
-                            backgroundColor={colors.primary[400]}
-                            p="30px"
-                        >
-                            <Typography variant="h5" fontWeight="600">
-                                Most Used Languages
-                            </Typography>
+                            {followers}
                             <Box
-                                display="flex"
-                                flexDirection="column"
-                                alignItems="center"
-                                mt="25px"
+                                gridColumn="span 4"
+                                gridRow="span 2"
+                                backgroundColor={colors.primary[400]}
+                                p="30px"
+                            >
+                                <Typography variant="h5" fontWeight="600">
+                                    Most Used Languages
+                                </Typography>
+                                <Box
+                                    display="flex"
+                                    flexDirection="column"
+                                    alignItems="center"
+                                    mt="25px"
+                                >
+                                    <Typography
+                                        variant="h5"
+                                        color={colors.greenAccent[500]}
+                                    >
+                                        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=anuraghazra&layout=donut&theme=prussian" alt="Top Languages Card" />
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Box
+                                gridColumn="span 4"
+                                gridRow="span 2"
+                                backgroundColor={colors.primary[400]}
                             >
                                 <Typography
                                     variant="h5"
-                                    color={colors.greenAccent[500]}
+                                    fontWeight="600"
+                                    sx={{ padding: "30px 30px 0 30px" }}
                                 >
-                                    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=anuraghazra&layout=donut&theme=prussian" alt="Top Languages Card" />
+                                    Data NXST
                                 </Typography>
+                                <Box height="250px" mt="-20px">
+                                </Box>
                             </Box>
-                        </Box>
-                        <Box
-                            gridColumn="span 4"
-                            gridRow="span 2"
-                            backgroundColor={colors.primary[400]}
-                        >
-                            <Typography
-                                variant="h5"
-                                fontWeight="600"
-                                sx={{ padding: "30px 30px 0 30px" }}
+                            <Box
+                                gridColumn="span 4"
+                                gridRow="span 2"
+                                backgroundColor={colors.primary[400]}
+                                padding="30px"
                             >
-                                Data NXST
-                            </Typography>
-                            <Box height="250px" mt="-20px">
-                            </Box>
-                        </Box>
-                        <Box
-                            gridColumn="span 4"
-                            gridRow="span 2"
-                            backgroundColor={colors.primary[400]}
-                            padding="30px"
-                        >
-                            <Typography
-                                variant="h5"
-                                fontWeight="600"
-                                sx={{ marginBottom: "15px" }}
-                            >
-                                DATA 3
-                            </Typography>
-                            <Box height="200px">
+                                <Typography
+                                    variant="h5"
+                                    fontWeight="600"
+                                    sx={{ marginBottom: "15px" }}
+                                >
+                                    DATA 3
+                                </Typography>
+                                <Box height="200px">
+                                </Box>
                             </Box>
                         </Box>
                     </Box>
-                </Box>
-            </ThemeProvider>
-        </ColorModeContext.Provider>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
         </div>
     );
 };
